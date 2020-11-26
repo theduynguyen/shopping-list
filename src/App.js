2d777be76cc23bc2778aa7bd00388b03aa7cd3cc
The Duy Nguyen <PiequeNguyen@googlemail.com>
2020-11-26 23:03:40 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Select from 'react-select'
 
-function App() {
+import database from './Database'
+
+import './App.scss'
+
+const RecipeList = props => {
+  const options = Object.keys(database).map(recipe => {
+    return { value: recipe, label: recipe }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Select
+        isMulti
+        name='Recipes'
+        options={options}
+        className='basic-multi-select'
+        classNamePrefix='select'
+        onChange={selected => props.setRecipes(selected)}
+      />
+    </main>
+  )
 }
 
-export default App;
+const IngredientListElement = props => {
+  return (
+    <main>
+      {props.ingredientList.map(x => (
+        <div>{x}</div>
+      ))}
+    </main>
+  )
+}
+
+const IngredientList = props => {
+  return (
+    <main>
+      {props.selectedRecipes.map(recipe => (
+        <IngredientListElement ingredientList={database[recipe.value]} />
+      ))}
+    </main>
+  )
+}
+
+const App = props => {
+  const [selectedRecipes, setSelectedRecipes] = useState([])
+
+  useEffect(() => {
+    console.log(selectedRecipes)
+  }, [selectedRecipes])
+
+  return (
+    <div className='App'>
+      <RecipeList setRecipes={setSelectedRecipes} />
+      <IngredientList selectedRecipes={selectedRecipes} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Database.js
@@ -0,0 +1,8 @@
+const database = {
+  Zucchinipuffer: ['a', 'b', 'c'],
+  'Spaghetti Bolognese': ['d', 'e', 'f'],
+  Couscous: ['g', 'h', 'i'],
+  Brot: ['e', 'p']
+}
+
+export default database
